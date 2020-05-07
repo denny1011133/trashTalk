d11fa19ad1a3ade9e0299134131a8d78a2e9cc6b
Denny <denny1011133@gmail.com>
2020-05-07 19:40:58 +0800

    trashTalk

---
+++ app.js
@@ -0,0 +1,28 @@
+// app.js
+// include packages and define server related variables
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser')
+const generate_talk = require('./generate_talk')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// setting routes
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.post('/', (req, res) => {
+  const professions = req.body
+  const trashTalk = generate_talk(professions)
+  res.render('index', { trashTalk: trashTalk })
+})
+// starts the express server and listening for connections.
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
